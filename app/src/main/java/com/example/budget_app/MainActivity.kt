8e63f96ad20ce52d3df8dd6e580c048ff239f67d
jessebennett <jb.jediknight@gmail.com>
2019-06-24 15:30:47 -0700

    Connect to HockeyApp for distribution.

--- app/src/main/java/com/example/budget_app/MainActivity.kt
+++ app/src/main/java/com/example/budget_app/MainActivity.kt
@@ -5,12 +5,19 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCenter.start(
+            application, "019a5c56-13b5-4917-ae15-6e2155ac1873",
+            Analytics::class.java, Crashes::class.java
+        )
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
