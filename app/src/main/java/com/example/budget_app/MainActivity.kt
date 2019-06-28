0ba46bad25d8b9e0bae5dc9fef5e46b861451802
jessebennett <jb.jediknight@gmail.com>
2019-06-28 11:45:07 -0700

    Create button that tells you when clicked.

--- app/src/main/java/com/example/budget_app/MainActivity.kt
+++ app/src/main/java/com/example/budget_app/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.budget_app
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 import com.microsoft.appcenter.distribute.Distribute
+import android.widget.Button
+import android.widget.TextView
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,28 +18,16 @@ class MainActivity : AppCompatActivity() {
             Analytics::class.java, Crashes::class.java, Distribute::class.java
         )
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "You Clicked!", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
+        setContentView(R.layout.content_main)
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        // Get the widgets
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        // Set a click listener for button widget
+        button.setOnClickListener{
+            textView.text = getString(R.string.text_clicked)
         }
     }
 }
