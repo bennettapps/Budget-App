114787a6ecb7f77b5177ab498b92965d3d3da0af
jessebennett <jb.jediknight@gmail.com>
2019-07-09 12:10:19 -0700

    Remove old inputs. Create list of categories (at this point you can't edit them) which can be added and removed.

--- app/src/main/java/com/example/budget_app/MainActivity.kt
+++ app/src/main/java/com/example/budget_app/MainActivity.kt
@@ -6,17 +6,16 @@ import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 import com.microsoft.appcenter.distribute.Distribute
-import android.widget.Button
-import android.widget.TextView
-import android.widget.EditText
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
-import com.crashlytics.android.answers.ContentViewEvent
-import com.crashlytics.android.answers.Answers
-
-
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.view.ViewGroup
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ll: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCenter.start(
@@ -28,34 +27,42 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.content_main)
 
-        // Get the widgets
-        val textView = findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button)
-        val inputField = findViewById<EditText>(R.id.editText)
-        val enterButton = findViewById<Button>(R.id.button2)
+        val button = findViewById<View>(R.id.floatingActionButton)
+        val button2 = findViewById<View>(R.id.floatingActionButton2)
 
-        // Set a click listener for button widget
-        button.setOnClickListener{
-            textView.text = getString(R.string.text_clicked)
-            Answers.getInstance().logContentView(
-                ContentViewEvent()
-                    .putContentName("Click")
-                    .putContentType("Button")
-                    .putContentId("0001")
-            )
+        ll = findViewById(R.id.linearLayout)
+
+        button.setOnClickListener {
+            createButton("New Category", "$60.00")
         }
 
-        var input = ""
-        enterButton.setOnClickListener{
-            input = inputField.text.toString()
-            enterButton.text = input
-            Answers.getInstance().logContentView(
-                ContentViewEvent()
-                    .putContentName("Input")
-                    .putContentType("Text")
-                    .putContentId("0002")
-                    .putCustomAttribute("Input Text", input)
-            )
+        button2.setOnClickListener {
+            deleteLastButton()
+        }
+    }
+
+    private fun createButton(categoryName: String, price: String) {
+        val newLayout = LinearLayout(this)
+        val categoryText = TextView(this)
+        val priceText = TextView(this)
+
+        ll.addView(newLayout)
+        newLayout.addView(categoryText)
+        newLayout.addView(priceText)
+
+        newLayout.orientation = LinearLayout.HORIZONTAL
+        newLayout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        categoryText.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        priceText.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        priceText.textAlignment = View.TEXT_ALIGNMENT_TEXT_END
+
+        categoryText.text = categoryName
+        priceText.text = price
+    }
+
+    private fun deleteLastButton() {
+        if(ll.childCount > 0) {
+            ll.removeViewAt(ll.childCount - 1)
         }
     }
 }
